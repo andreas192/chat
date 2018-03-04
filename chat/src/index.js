46a6a427e717e5d3d072575649deb7370ca44689
andreiluculescu <andrei.luculescu@dpdgroup.co.uk>
2018-03-04 20:43:43 +0200

    Working single user chat

---
+++ chat/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import chat from './reducers';
+import { addUser } from './actions'
+
+const store = createStore(chat);
+
+store.dispatch(addUser('Me'));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+registerServiceWorker();
